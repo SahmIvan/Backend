1b7aa1b4f8adfc53adcdd79b3ede29197a081446
ivan <ivan_gtm@outlook.com>
2025-10-16 20:11:27 -0600

    Si

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# Ruta raíz para comprobar que el servidor está vivo
+
 @app.route("/", methods=["GET"])
 def root():
     return jsonify({"ok": True, "message": "Backend Flask levantado. Usa /action para GET/POST."})
 
-# /action acepta GET y POST
+
 @app.route("/action", methods=["GET", "POST"])
 def action():
     if request.method == "GET":
         return jsonify({"ok": True, "message": "GET recibido en /action"})
-    # POST
+    
     data = request.get_json(silent=True) or {}
     return jsonify({"ok": True, "message": "POST recibido en /action", "received": data})
 
